143a11ab31a4bd1b203d79622f63bec3343ae4d2
apolumestny <anpolumestny@gmail.com>
2023-09-10 23:10:27 +0300

    test genderalize

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,19 @@
 from functions.level_1.one_gender import genderalize
 
 
-def test_genderalize():
-    pass
+def test_genderalize_gender_male():
+    result = genderalize('table', 'moon', 'male')
+    expected_result = 'table'
+    assert result == expected_result
+
+
+def test_genderalize_gender_not_male():
+    result = genderalize('table', 'moon', 'female')
+    expected_result = 'moon'
+    assert result == expected_result
+
+
+def test_genderalize_gender_none():
+    result = genderalize('table', 'moon', gender=None)
+    expected_result = 'moon'
+    assert result == expected_result
