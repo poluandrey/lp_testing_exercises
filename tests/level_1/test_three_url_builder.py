e5e284042dbfc7e6278184752df9d19408e92ae6
apolumestny <anpolumestny@gmail.com>
2023-09-11 11:11:07 +0300

    test url builder

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,5 +1,14 @@
 from functions.level_1.three_url_builder import build_url
 
 
-def test_build_url():
-    pass
+def test_build_url__without_params():
+    assert build_url('example.com', 'home') == 'example.com/home', 'url_builder without params work incorrectly'
+
+
+def test_build_url__with_one_params():
+    assert build_url('example.com', 'home', {'username': 'Andrey'}) == 'example.com/home?username=Andrey', 'url_builder with one param work incorrectly'
+
+
+def test_build_url__with_few_params():
+    assert build_url('example.com', 'home', {'username': 'Andrey', 'group': 'my_group'}) == 'example.com/home?username=Andrey&group=my_group', 'url builder with few params work incorrectly'
+
